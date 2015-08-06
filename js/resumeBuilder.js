15f0c7be3566a095d16fb0265145954b5a2aeec5
jbuechs <janina.buechner@gmail.com>
2015-08-06 15:48:34 -0400

    refactor separate view and model education

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -75,7 +75,52 @@ var model = {
 			'images' : ['images/portfolio1.png', 'images/portfolio2.png']
 		}
 	],
-	'schools' : {},
+	'schools' : [
+		{
+			'name' : 'Drexel University',
+			'location' : 'Philadelphia, PA',
+			'degree' : 'MS in Learning Technologies',
+			'majors' : ['Learning in Game-Based Environments'],
+			'dates' : 2013,
+			'url' : 'http://www.drexel.com/online-degrees/education-degrees/ms-learningtech/index.aspx'
+		},
+		{
+			'name' : 'University of Virginia',
+			'location' : 'Charlottesville, VA',
+			'degree' : 'Masters in Teaching',
+			'majors' : ['Elementary Education (k-8)'],
+			'dates' : 2002,
+			'url' : 'http://curry.virginia.edu/academics/degrees/master-of-teaching/master-of-teaching-elementary-education'
+		},
+		{
+			'name' : 'University of Virginia',
+			'location' : 'Charlottesville, VA',
+			'degree' : 'BA',
+			'majors' : ['Psychology'],
+			'dates' : 2002,
+			'url' : 'http://college.artsandsciences.virginia.edu/degreeprograms'
+		}
+	],
+	'onlineCourses' : [
+		{
+			'title' : 'Introduction to Java Programming',
+			'school' : 'Udacity',
+			'dates' : 2015,
+			'url' : 'https://www.udacity.com/course/intro-to-java-programming--cs046'
+		},
+		{
+			'title' : 'Introduction to HTML and CSS',
+			'school' : 'Udacity',
+			'dates' : 2015,
+			'url' : 'https://www.udacity.com/course/intro-to-html-and-css--ud304'
+		},
+				{
+			'title' : 'An Introduction to Interactive Programming in Python',
+			'school' : 'Coursera',
+			'dates' : 2015,
+			'url' : 'https://www.coursera.org/course/interactivepython1'
+		}
+	],
 };
 
 var view = {
@@ -144,112 +189,63 @@ var view = {
 			}
 		}
 	},
-};
-
-var octopus = {
-	init : function() {
-		// Header
-		view.displayBio(model.bio);
-		view.displayContacts('#topContacts', model.bio);
-		view.displayContacts('#footerContacts', model.bio);
-		// Employment
-		view.displayJobs(model.jobs);
-		// Projects
-		view.displayProjects(model.projects);
-	},
-};
-
-
-var education = {
-	'schools' : [
-		{
-			'name' : 'Drexel University',
-			'location' : 'Philadelphia, PA',
-			'degree' : 'MS in Learning Technologies',
-			'majors' : ['Learning in Game-Based Environments'],
-			'dates' : 2013,
-			'url' : 'http://www.drexel.com/online-degrees/education-degrees/ms-learningtech/index.aspx'
-		},
-		{
-			'name' : 'University of Virginia',
-			'location' : 'Charlottesville, VA',
-			'degree' : 'Masters in Teaching',
-			'majors' : ['Elementary Education (k-8)'],
-			'dates' : 2002,
-			'url' : 'http://curry.virginia.edu/academics/degrees/master-of-teaching/master-of-teaching-elementary-education'
-		},
-		{
-			'name' : 'University of Virginia',
-			'location' : 'Charlottesville, VA',
-			'degree' : 'BA',
-			'majors' : ['Psychology'],
-			'dates' : 2002,
-			'url' : 'http://college.artsandsciences.virginia.edu/degreeprograms'
-		}
-	],
-	'onlineCourses' : [
-		{
-			'title' : 'Introduction to Java Programming',
-			'school' : 'Udacity',
-			'dates' : 2015,
-			'url' : 'https://www.udacity.com/course/intro-to-java-programming--cs046'
-		},
-		{
-			'title' : 'Introduction to HTML and CSS',
-			'school' : 'Udacity',
-			'dates' : 2015,
-			'url' : 'https://www.udacity.com/course/intro-to-html-and-css--ud304'
-		},
-				{
-			'title' : 'An Introduction to Interactive Programming in Python',
-			'school' : 'Coursera',
-			'dates' : 2015,
-			'url' : 'https://www.coursera.org/course/interactivepython1'
-		}
-	],
-	display : function () {
-		if (education.schools.length > 0) {
-			for (var i = 0; i < education.schools.length; i++) {
+	displayEducation : function(schools, onlineCourses) {
+		var i, formattedDate;
+		if (schools.length > 0) {
+			for (i = 0; i < schools.length; i++) {
 				$('#education').append(HTMLschoolStart);
-				var formattedName = HTMLschoolName.replace('%data%', education.schools[i].name);
-				formattedName = formattedName.replace('#', education.schools[i].url);
-				var formattedDegree = HTMLschoolDegree.replace('%data%', education.schools[i].degree);
-				var formattedDate = HTMLschoolDates.replace('%data%', education.schools[i].dates);
-				var formattedLocation = HTMLschoolLocation.replace('%data%', education.schools[i].location);
+				var formattedName = HTMLschoolName.replace('%data%', schools[i].name);
+				formattedName = formattedName.replace('#', schools[i].url);
+				var formattedDegree = HTMLschoolDegree.replace('%data%', schools[i].degree);
+				formattedDate = HTMLschoolDates.replace('%data%', schools[i].dates);
+				var formattedLocation = HTMLschoolLocation.replace('%data%', schools[i].location);
 				$('.education-entry:last').append(formattedName);
 				$('.education-entry:last').append(formattedDegree);
 				$('.education-entry:last').append(formattedDate);
 				$('.education-entry:last').append(formattedLocation);
-				if (education.schools[i].majors.length > 0) {
-					for (var j = 0; j < education.schools[i].majors.length; j++) {
-						var formattedMajor = HTMLschoolMajor.replace('%data%', education.schools[i].majors[j]);
+				if (schools[i].majors.length > 0) {
+					for (var j = 0; j < schools[i].majors.length; j++) {
+						var formattedMajor = HTMLschoolMajor.replace('%data%', schools[i].majors[j]);
 						$('.education-entry:last').append(formattedMajor);
 					}
 				}
 			}
 		}
 		$('#education').append(HTMLonlineClasses);
-		if (education.onlineCourses.length > 0) {
-			for (var i = 0; i < education.onlineCourses.length; i++) {
+		if (onlineCourses.length > 0) {
+			for (i = 0; i < onlineCourses.length; i++) {
 				$('#education').append(HTMLschoolStart);
-				var formattedTitle = HTMLonlineTitle.replace('%data%', education.onlineCourses[i].title);
-				formattedTitle = formattedTitle.replace('#', education.onlineCourses[i].url);
-				var formattedSchool = HTMLonlineSchool.replace('%data%', education.onlineCourses[i].school);
-				var formattedDate = HTMLonlineDates.replace('%data%', education.onlineCourses[i].dates);
+				var formattedTitle = HTMLonlineTitle.replace('%data%', onlineCourses[i].title);
+				formattedTitle = formattedTitle.replace('#', onlineCourses[i].url);
+				var formattedSchool = HTMLonlineSchool.replace('%data%', onlineCourses[i].school);
+				formattedDate = HTMLonlineDates.replace('%data%', onlineCourses[i].dates);
 				$('.education-entry:last').append(formattedTitle);
 				$('.education-entry:last').append(formattedSchool);
 				$('.education-entry:last').append(formattedDate);
 			}
 		}
+	},
+	displayMap : function() {
+		$('#mapDiv').append(googleMap);
 	}
 };
 
+var octopus = {
+	init : function() {
+		// Header
+		view.displayBio(model.bio);
+		// Contacts
+		view.displayContacts('#topContacts', model.bio);
+		view.displayContacts('#footerContacts', model.bio);
+		// Employment
+		view.displayJobs(model.jobs);
+		// Projects
+		view.displayProjects(model.projects);
+		// Education
+		view.displayEducation(model.schools, model.onlineCourses);
+		// Map
+		view.displayMap();
+	},
+};
+
 octopus.init();
-
-//Education
-education.display();
-
-//Map
-$('#mapDiv').append(googleMap);
-
-// Contacts
